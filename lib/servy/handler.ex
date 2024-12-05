839700daf7ed37542d83c06dabcc2ac27f8c22ae
MMcClure11 <mmcclure0100@gmail.com>
2024-12-05 17:09:00 -0600

    feat: advanced pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -16,29 +16,48 @@ defmodule Servy.Handler do
       |> List.first()
       |> String.split(" ")
 
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
-    %{conv | resp_body: "Bears, Liöns, Tigers"}
+    %{conv | status: 200, resp_body: "Bears, Liöns, Tigers"}
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
-    %{conv | resp_body: "Winnie, Paddington, Smokey"}
+    %{conv | status: 200, resp_body: "Winnie, Paddington, Smokey"}
   end
 
-  def route(%{method: "GET", path: "/bigfoot"} = conv) do
-    %{conv | resp_body: "Yeti, Sasquatch"}
+  def route(%{method: "GET", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bears id #{id}"}
+  end
+
+  def route(%{method: "DELETE", path: "/bears/" <> id} = conv) do
+    %{conv | status: 200, resp_body: "Bears id #{id} deleted"}
+  end
+
+  def route(%{method: _method, path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{byte_size(conv.resp_body)}
 
     #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
 
@@ -82,7 +101,18 @@ response = Servy.Handler.handle(request)
 IO.puts(response)
 
 request = """
-GET /bigfoot HTTP/1.1
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts(response)
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
