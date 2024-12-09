8dcceecbbf442a811989a1fe2dc90b119c5bbfa5
MMcClure11 <mmcclure0100@gmail.com>
2024-12-09 19:09:17 +0100

    feat: recursion, triple all the numbers exercise v2

--- lib/servy/recurse.ex
+++ lib/servy/recurse.ex
@@ -12,13 +12,13 @@ defmodule Recurse do
 
   def sum([], total), do: IO.puts("Total: #{total}")
 
-  def triple([head | tail], triple) do
-    triple(tail, triple ++ [head * 3])
+  def triple([head | tail]) do
+    [head * 3 | triple(tail)]
   end
 
-  def triple([], triple), do: dbg(triple)
+  def triple([]), do: []
 end
 
 Recurse.loopy([1, 2, 3, 4, 5])
 Recurse.sum([1, 2, 3, 4, 5], 0)
-Recurse.triple([1, 2, 3, 4, 5], [])
+IO.inspect(Recurse.triple([1, 2, 3, 4, 5]))
