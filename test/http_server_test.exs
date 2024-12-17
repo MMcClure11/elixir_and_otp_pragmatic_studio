79385f81e34fe7cc1acdf3ebe7ff081621cce3e3
MMcClure11 <mmcclure0100@gmail.com>
2024-12-17 13:35:10 +0100

    feat: Asynchronous Tasks, exercise 1
    Change HttpServerTest to Use Tasks

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -4,23 +4,17 @@ defmodule HttpSeverTest do
   alias Servy.HttpServer
 
   test "accepts a request on a socket and sends back a response" do
-    spawn(fn -> HttpServer.start(4000) end)
-    parent = self()
-    max_concurrent_requests = 5
+    spawn(HttpServer, :start, [4000])
+    url = "http://localhost:4000/wildthings"
 
-    for _ <- 1..max_concurrent_requests do
-      spawn(fn ->
-        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
-        send(parent, {:response, response})
-      end)
-    end
+    1..5
+    |> Enum.map(fn _ -> Task.async(fn -> HTTPoison.get(url) end) end)
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
+  end
 
-    for _ <- 1..max_concurrent_requests do
-      receive do
-        {:response, response} ->
-          assert response.status_code == 200
-          assert response.body == "Bears, Lions, Tigers"
-      end
-    end
+  defp assert_successful_response({:ok, response}) do
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 end
