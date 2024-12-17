d4027f014dfce721a6c9c8a535f87dff67cec977
MMcClure11 <mmcclure0100@gmail.com>
2024-12-17 11:12:54 +0100

    feat: Sending and Receiving Messages, exercise 3
    Use HTTPoison In Your HttpServerTest

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -2,27 +2,11 @@ defmodule HttpSeverTest do
   use ExUnit.Case, async: true
 
   alias Servy.HttpServer
-  alias Servy.HttpClient
 
   test "accepts a request on a socket and sends back a response" do
     spawn(fn -> HttpServer.start(4000) end)
-
-    request = """
-    GET /wildthings HTTP/1.1\r
-    Host: example.com\r
-    User-Agent: ExampleBrowser/1.0\r
-    Accept: */*\r
-    \r
-    """
-
-    response = HttpClient.send_request(request)
-
-    assert response == """
-           HTTP/1.1 200 OK\r
-           Content-Type: text/html\r
-           Content-Length: 20\r
-           \r
-           Bears, Lions, Tigers
-           """
+    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
 end
