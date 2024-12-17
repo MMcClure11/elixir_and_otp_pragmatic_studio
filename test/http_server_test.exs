59797b98eb74961738e975532800fbd0acbe8fe4
MMcClure11 <mmcclure0100@gmail.com>
2024-12-17 11:28:12 +0100

    feat: Sending and Receiving Messages, exercise 4
    Change HttpServerTest to Send Concurrent Requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -5,8 +5,22 @@ defmodule HttpSeverTest do
 
   test "accepts a request on a socket and sends back a response" do
     spawn(fn -> HttpServer.start(4000) end)
-    {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
-    assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
+    parent = self()
+    max_concurrent_requests = 5
+
+    for _ <- 1..max_concurrent_requests do
+      spawn(fn ->
+        {:ok, response} = HTTPoison.get("http://localhost:4000/wildthings")
+        send(parent, {:response, response})
+      end)
+    end
+
+    for _ <- 1..max_concurrent_requests do
+      receive do
+        {:response, response} ->
+          assert response.status_code == 200
+          assert response.body == "Bears, Lions, Tigers"
+      end
+    end
   end
 end
